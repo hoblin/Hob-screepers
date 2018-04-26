a44e9b035f531ab63a865e186d064ec3436ee4dc
hoblin <evgeny.gurin@gmail.com>
2018-04-26 22:49:49 +0300

    Jerks will set signs on all roons

--- role.jerk.js
+++ role.jerk.js
@@ -22,22 +22,20 @@ function role(creep) {
             range: 23
         });
         if (Game.time % 100) creep.memory.destinationReached = true;
-        if (creep.pos.roomName === creep.memory.destination) {
-            if (!creep.handleMilitaryCreep(false, false, false, true)) {
-                if (creep.room.controller && (!creep.room.controller.sign || creep.room.controller.sign.username !== USERNAME) &&
-                    (!creep.room.controller.owner || !_.includes(FRIENDLIES, creep.room.controller.owner.username)) &&
-                    (!creep.room.controller.reservation || !_.includes(FRIENDLIES, creep.room.controller.reservation.username))) {
-                    let signs = ["This one will be mine.", "I'll eat that.", "Om-nom-nom", "I want this one", "MOAR ROOMS!!!1!1!"];
-                    switch (creep.signController(creep.room.controller, _.sample(signs))) {
-                        case OK:
-                            creep.memory.destinationReached = true;
-                            break;
-                        case ERR_NOT_IN_RANGE:
-                            creep.shibMove(creep.room.controller, {offRoad: true});
-                    }
-                } else {
-                    creep.memory.destinationReached = true;
+        if (!creep.handleMilitaryCreep(false, false, false, true)) {
+            if (creep.room.controller && (!creep.room.controller.sign || creep.room.controller.sign.username !== USERNAME) &&
+                (!creep.room.controller.owner || !_.includes(FRIENDLIES, creep.room.controller.owner.username)) &&
+                (!creep.room.controller.reservation || !_.includes(FRIENDLIES, creep.room.controller.reservation.username))) {
+                let signs = ["This one will be mine.", "I'll eat that.", "Om-nom-nom", "I want this one", "MOAR ROOMS!!!1!1!"];
+                switch (creep.signController(creep.room.controller, _.sample(signs))) {
+                    case OK:
+                        creep.memory.destinationReached = true;
+                        break;
+                    case ERR_NOT_IN_RANGE:
+                        creep.shibMove(creep.room.controller, {offRoad: true});
                 }
+            } else {
+                creep.memory.destinationReached = true;
             }
         }
     } else {
